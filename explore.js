3b6c1e968c67e62c4319142e364b5db5d7e397fb
nikhil1028 <nikhil.raj.cse13@itbhu.ac.in>
2017-01-06 14:34:04 +0530

    Explore changes
    First explore changes according to new deesign.. Mohit's design.

--- explore.js
+++ explore.js
@@ -1,10 +1,39 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-	document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+    var div = document.getElementById('myNav');
+    if (div.style.display !== 'block') {
+        div.style.display = 'block';
+    }
+    else {
+        div.style.display = 'none';
+    }
 }
 
-/* Close when someone clicks on the "x" symbol inside the explore */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-	document.getElementsByTagName("body")[0].style.overflowY = "scroll";
+function myfunction1(){
+	var div = document.getElementById('locationInput');
+    if (div.style.display !== 'block') {
+        div.style.display = 'block';
+    }
+    else {
+        div.style.display = 'none';
+    }
+}
+
+function myfunction2() {
+	var div = document.getElementById('fundingInput');
+    if (div.style.display !== 'block') {
+        div.style.display = 'block';
+    }
+    else {
+        div.style.display = 'none';
+    }
+}
+
+function myfunction3(){
+	var div = document.getElementById('causeInput');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
 }
